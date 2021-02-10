0d353aa9b11e91110cc2010116189f2bbac14878
Composera <asadxoja5651@gmail.com>
2021-02-10 12:26:23 +0500

    fixed wishlists error

--- app/Http/Resources/WishlistCollection.php
+++ app/Http/Resources/WishlistCollection.php
@@ -11,23 +11,22 @@ class WishlistCollection extends ResourceCollection
         return [
             'data' => $this->collection->map(function($data) {
                 if($data->product) {
-                return [
-                    'id' => (integer) $data->id,
-                        'product_id' => $data->product->id,
-                        'name' => $data->product->name,
-                        'thumbnail_image' => api_asset($data->product->thumbnail_img),
-                        'base_price' => (double) homeBasePrice($data->product->id),
-                        'base_discounted_price' => (double) homeDiscountedBasePrice($data->product->id),
-                        'unit' => $data->product->unit,
-                        'rating' => (double) $data->product->rating,
-                        'links' => [
-                            'details' => route('products.show', $data->product->id),
-                            'reviews' => route('api.reviews.index', $data->product->id),
-                            'related' => route('products.related', $data->product->id),
-                            'top_from_seller' => route('products.topFromSeller', $data->product->id)
-                        ]
-                    ]
-                ];
+                    return [
+                        'id' => (integer) $data->id,
+                            'product_id' => $data->product->id,
+                            'name' => $data->product->name,
+                            'thumbnail_image' => api_asset($data->product->thumbnail_img),
+                            'base_price' => (double) homeBasePrice($data->product->id),
+                            'base_discounted_price' => (double) homeDiscountedBasePrice($data->product->id),
+                            'unit' => $data->product->unit,
+                            'rating' => (double) $data->product->rating,
+                            'links' => [
+                                'details' => route('products.show', $data->product->id),
+                                'reviews' => route('api.reviews.index', $data->product->id),
+                                'related' => route('products.related', $data->product->id),
+                                'top_from_seller' => route('products.topFromSeller', $data->product->id)
+                            ]
+                        ];
                 }
             })
         ];
