dd771ad4f1716b36f9e46add1bb95fee47113c82
Asadxoja <asadxoja5651@gmail.com>
2021-02-15 10:55:16 +0500

    ADded seller controller

---
+++ app/Http/Controllers/Api/SellerController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Seller;
+use Illuminate\Http\Request;
+
+class SellerController extends Controller
+{
+    public function sellers()
+    {
+        return response()->json([
+            'sellers' => Seller::all()
+        ], 200);
+    }
+
+    public function seller($id)
+    {
+        $seller = Seller::where('id', $id)->with(['products'])->firstOrFail();
+
+        return response()->json([
+            'seller' => $seller
+        ], 200);
+    }
+
+    public function topSelling($id)
+    {
+        $seller = Seller::findOrFail($id);
+        $products = \App\Product::where('user_id', $seller->user_id)
+            ->where('published', 1)
+            ->orderBy('num_of_sale', 'desc')
+            ->paginate(24);
+
+        return response()->json([
+            'products' => $products
+        ], 200);
+    }
+
+    public function featuredProducts($id)
+    {
+        $seller = Seller::findOrFail($id);
+        $products = \App\Product::where('user_id', $seller->user_id)
+            ->where('published', 1)
+            ->orderBy('created_at', 'desc'  )
+            ->paginate(24);
+
+        return response()->json([
+            'products' => $products
+        ], 200);
+    }
+
+    public function allProducts($id)
+    {
+        $seller = Seller::findOrFail($id);
+        $products = \App\Product::where('user_id', $seller->user_id)
+            ->where('published', 1)
+            ->paginate(24);
+
+        return response()->json([
+            'products' => $products
+        ], 200);
+    }
+}
