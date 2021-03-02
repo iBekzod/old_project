e2fd9a8e4bdc0d3b99cf25bcfe866ccd899a1279
iBekzod <erkinovbegzod.45@gmail.com>
2021-03-02 12:21:28 +0500

    Observer added

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -6,6 +6,8 @@ use Illuminate\Support\Facades\Event;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+use App\Order;
+use App\Observers\OrderObserver;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -28,7 +30,7 @@ class EventServiceProvider extends ServiceProvider
   public function boot()
   {
     parent::boot();
-
+    Order::observe(OrderObserver::class);
     //
   }
 }
