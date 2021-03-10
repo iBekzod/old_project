54c8677a499272455756a62eb5c38cd3875949ee
Asadxoja <asadxoja5651@gmail.com>
2021-03-10 18:00:05 +0500

    seller pages update

--- app/Http/Controllers/RefundRequestController.php
+++ app/Http/Controllers/RefundRequestController.php
@@ -47,7 +47,7 @@ class RefundRequestController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
     public function vendor_index()
     {
