66322d718f7dd4b53870cc2c2fc95b3789f7eeff
iBekzod <erkinovbegzod.45@gmail.com>
2021-03-12 16:41:16 +0500

    brand added

--- routes/api.php
+++ routes/api.php
@@ -88,7 +88,7 @@ Route::prefix('v1')->group(function () {
     Route::get('products/category/{id}', 'Api\ProductController@category')->name('api.products.category');
     Route::get('products/sub-category/{id}', 'Api\ProductController@subCategory')->name('products.subCategory');
     Route::get('products/sub-sub-category/{id}', 'Api\ProductController@subSubCategory')->name('products.subSubCategory');
-    Route::get('products/brand/{id}', 'Api\ProductController@brand')->name('api.products.brand');
+    Route::get('products/brand/{id}/{brand_id}', 'Api\ProductController@brand')->name('api.products.brand');
     Route::get('products/todays-deal', 'Api\ProductController@todaysDeal');
     Route::get('products/flash-deal', 'Api\ProductController@flashDeal');
     Route::get('products/featured', 'Api\ProductController@featured');
