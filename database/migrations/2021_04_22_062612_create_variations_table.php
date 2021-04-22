ecd3e882d6abc3e0917d9b618da99d0e24465913
iBekzod <erkinovbegzod.45@gmail.com>
2021-04-22 11:33:44 +0500

    migrations added and generated

---
+++ database/migrations/2021_04_22_062612_create_variations_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVariationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('variations', function (Blueprint $table) {
+            $table->integer('id', true);
+            $table->integer('product_id');
+            $table->string('name', 255)->nullable();
+            $table->integer('todays_deal')->nullable();
+            $table->string('image', 255)->nullable();
+            $table->string('sku', 255)->nullable();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('variations');
+    }
+}
