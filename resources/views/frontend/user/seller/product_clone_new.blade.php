b3f7d2edc772ff8d220ae83b0d683c92ca9f8daf
Shohruh04 <shohruhqudratullaev2455@gmail.com>
2021-04-24 14:43:03 +0500

    delete

--- resources/views/frontend/user/seller/product_clone_new.blade.php
+++ resources/views/frontend/user/seller/product_clone_new.blade.php
@@ -148,8 +148,6 @@
                                 <li class="page-item active" aria-current="page"><span class="page-link">1</span></li>
                                 <li class="page-item"><a class="page-link" href="#">2</a></li>
                                 <li class="page-item"><a class="page-link" href="#">3</a></li>
-
-
                                 <li class="page-item">
                                     <a class="page-link" href="#" rel="next" aria-label="Next »">›</a>
                                 </li>
