28325ba313c1815af5afaf84cf4c489d12d70663
Shohruh04 <shohruhqudratullaev2455@gmail.com>
2021-05-01 17:25:17 +0500

    added elements table

--- resources/views/backend/product-attributes/edit.blade.php
+++ resources/views/backend/product-attributes/edit.blade.php
@@ -156,11 +156,6 @@
                                                                     id="category_id"
                                                                     data-live-search="true" required> --}}
                                                                     @foreach ($categories as $category)
-                                                                        {{-- <option disabled
-                                                                        @if (in_array($category->id, $selected_categories))
-                                                                        selected
-                                                                        @endif
-                                                                        value="{{ $category->id }}">{{ $category->getTranslation('name') }}</option> --}}
                                                                         @foreach ($category->children as $childCategory)
                                                                             @include('backend.product-attributes.components.child_category',
                                                                             ['child_category'
@@ -205,6 +200,165 @@
 
 
     </div>
+
+    {{-- new --}}
+    <div style="overflow-y: scroll; ">
+        <table class="table table-bordered" style="width:1800px">
+            <thead>
+                <tr>
+                    <td class="text-center">
+                        <label for="" class="control-label">{{ translate('Slug') }}</label>
+                    </td>
+                    <td class="text-center">
+                        <label for="" class="control-label">{{ translate('Price') }}</label>
+                    </td>
+                    <td class="text-center">
+                        <label for="" class="control-label">{{ translate('crwd') }}</label>
+                    </td>
+                    <td class="text-center">
+                        <label for="" class="control-label">{{ translate('Quantity') }}</label>
+                    </td>
+                    <td class="text-center">
+                        <label for="" class="control-label">{{ translate('Discount') }}</label>
+                    </td>
+                    <td class="text-center">
+                        <label for="" class="control-label">{{ translate('Discount Type') }}</label>
+                    </td>
+                    <td class="text-center">
+                        <label for="" class="control-label">{{ translate('Delivery') }}</label>
+                    </td>
+
+                    <td class="text-center">
+                        <label for="" class="control-label">{{ translate('Tax') }}</label>
+                    </td>
+                    <td class="text-center">
+                        <label for="" class="control-label">{{ translate('Tax type') }}</label>
+                    </td>
+                    <td class="text-center">
+                        <label for="" class="control-label">{{ translate('Todays deels') }}</label>
+                    </td>
+                    <td class="text-center">
+                        <label for="" class="control-label">{{ translate('Published') }}</label>
+                    </td>
+                </tr>
+                <th>Hello World!</th>
+                <th>
+                    <input type="text" class="form-control" id="exampleInputEmail1">
+                </th>
+                <th>
+                    <select class="form-control aiz-selectpicker" name="select1" id="select1">
+                        <option value="1">Erkaklar Kiyimi</option>
+                        <option value="2">Ayolar Kiyimi</option>
+                        <option value="2">Bollalar Kiyimi</option>
+                    </select>
+                </th>
+                <th>
+                    <input type="text" class="form-control" id="exampleInputEmail1">
+                </th>
+                <th>
+                    <input type="text" class="form-control" id="exampleInputEmail1">
+                </th>
+                <th>
+                    <select class="form-control aiz-selectpicker" name="select1" id="select1">
+                        <option value="1">Erkaklar Kiyimi</option>
+                        <option value="2">Ayolar Kiyimi</option>
+                        <option value="2">Bollalar Kiyimi</option>
+                    </select>
+                </th>
+                <th>
+                    <select class="form-control aiz-selectpicker" name="select1" id="select1">
+                        <option value="1">Erkaklar Kiyimi</option>
+                        <option value="2">Ayolar Kiyimi</option>
+                        <option value="2">Bollalar Kiyimi</option>
+                    </select>
+                </th>
+                <th>
+                    <input type="text" class="form-control" id="exampleInputEmail1">
+                </th>
+                <th>
+                    <select class="form-control aiz-selectpicker" name="select1" id="select1">
+                        <option value="1">Erkaklar Kiyimi</option>
+                        <option value="2">Ayolar Kiyimi</option>
+                        <option value="2">Bollalar Kiyimi</option>
+                    </select>
+                </th>
+                <th>
+                    <label class="switch">
+                        <input type="checkbox">
+                        <span class="slider round"></span>
+                    </label>
+                </th>
+                <th>
+                    <label class="switch">
+                        <input type="checkbox">
+                        <span class="slider round"></span>
+                    </label>
+                </th>
+            </thead>
+        </table>
+    </div>
+    <style>
+        .switch {
+          position: relative;
+          display: inline-block;
+          width: 60px;
+          height: 34px;
+        }
+
+        .switch input {
+          opacity: 0;
+          width: 0;
+          height: 0;
+        }
+
+        .slider {
+          position: absolute;
+          cursor: pointer;
+          top: 0;
+          left: 0;
+          right: 0;
+          bottom: 0;
+          background-color: #ccc;
+          -webkit-transition: .4s;
+          transition: .4s;
+        }
+
+        .slider:before {
+          position: absolute;
+          content: "";
+          height: 26px;
+          width: 26px;
+          left: 4px;
+          bottom: 4px;
+          background-color: white;
+          -webkit-transition: .4s;
+          transition: .4s;
+        }
+
+        input:checked + .slider {
+          background-color: #2196F3;
+        }
+
+        input:focus + .slider {
+          box-shadow: 0 0 1px #2196F3;
+        }
+
+        input:checked + .slider:before {
+          -webkit-transform: translateX(26px);
+          -ms-transform: translateX(26px);
+          transform: translateX(26px);
+        }
+
+        /* Rounded sliders */
+        .slider.round {
+          border-radius: 34px;
+        }
+
+        .slider.round:before {
+          border-radius: 50%;
+        }
+    </style>
+
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script type="text/javascript">
         // $(document).ready(function() {
@@ -225,18 +379,19 @@
         //         event.preventDefault();
         //     });
         // });
-        $(document).ready(function(){
-    $("form").on("submit", function(event){
-        event.preventDefault();
+        $(document).ready(function() {
+            $("form").on("submit", function(event) {
+                event.preventDefault();
 
-        var formValues= $(this).serialize();
-        var actionUrl = $(this).attr("action");
+                var formValues = $(this).serialize();
+                var actionUrl = $(this).attr("action");
 
-        $.post(actionUrl, formValues, function(data){
-            // Display the returned data in browser
-            console.log(data)
-        });
-    });
-})
+                $.post(actionUrl, formValues, function(data) {
+                    // Display the returned data in browser
+                    console.log(data)
+                });
+            });
+        })
+
     </script>
 @endsection
