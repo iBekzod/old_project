25598e7acc5fc0306709d7a85ad7e6ee2f1676b1
iBekzod <erkinovbegzod.45@gmail.com>
2021-05-23 16:46:49 +0500

    country bug fixed

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -92,7 +92,7 @@ class ProductController extends Controller
     public function admin()
     {
         if($variations= Variation::where('lowest_price_id','<>', null)->get()){
-            return new ProductCollection( $variations);
+            return new ProductCollection($variations);
         }
         return null;
         // return Product::where('added_by', 'admin')->inRandomOrder()->paginate(10);
