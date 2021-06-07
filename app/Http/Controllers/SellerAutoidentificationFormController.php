d291446e955644e97f58c5cd33ec45969e939470
Alijon27011995 <muxtorova227@gmail.com>
2021-06-07 19:40:51 +0500

    add information

--- app/Http/Controllers/SellerAutoidentificationFormController.php
+++ app/Http/Controllers/SellerAutoidentificationFormController.php
@@ -8,8 +8,27 @@ class SellerAutoidentificationFormController extends Controller
 {
    public function seller_autoidentification_form_save(Request $request)
    {
-        //  dd($request->all());
-        return view('frontend.user.seller.seller_documentation');
+        return view('frontend.user.seller.seller_documentation', ['array'=>$request->all()]);
+
+
+//         array:16 [▼
+//   "_token" => "mKwvvtIilbaECnk7ori22QVVNNaquf2ycAGzIaXu"
+//   "Форма_собственности" => "Lorem at fuga Animi"
+//   "Юридическое_название_вендора" => "Quod iure impedit a"
+//   "Название_магазина" => "Quis quos ex rerum v"
+//   "Адрес_регистрации_вендора" => "Illo in ut magna tem"
+//   "Физический_адрес_вендора" => "Et unde sunt nostrud"
+//   "ИНН" => "26"
+//   "Название_банка" => "Commodo non fugiat"
+//   "МФО_банка" => "9"
+//   "Р/С" => "33"
+//   "ФИО_директора" => "Eos ea ratione quo e"
+//   "Тел_директора" => "55"
+//   "Электронная_почта_директора" => "tahahe@mailinator.com"
+//   "ФИО_менеджера_ответственного_за_сотрудничество" => "11"
+//   "Тел_менеджера" => "55"
+//   "Электронная_почта_менеджера" => "lifi@mailinator.com"
+// ]
    }
 
 
