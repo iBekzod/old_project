2121df6969a2ceb6f3cb796ec9e90a49a03a7345
Alijon27011995 <muxtorova227@gmail.com>
2021-06-12 19:25:40 +0500

    controller

--- app/Http/Controllers/SellerAutoidentificationFormController.php
+++ app/Http/Controllers/SellerAutoidentificationFormController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Seller;
+use App\SellerAutoidntification;
 use Illuminate\Http\Request;
 
 class SellerAutoidentificationFormController extends Controller
@@ -9,8 +11,15 @@ class SellerAutoidentificationFormController extends Controller
    public function seller_autoidentification_form_save(Request $request)
    {
         $array=$request->all();
-       // dd($array);
-         return view('frontend.user.seller.seller_autoidentification')->with('seller', $array);
+        dd($array);
+
+        $request->validate([
+            // TODO::tak seller navicatdatoldirish kere inn validate save and login register pdf to html
+        ]);
+
+          $seller= new Seller;
+
+        //  return view('frontend.user.seller.seller_autoidentification')->with('seller', $array);
 
 
    }
