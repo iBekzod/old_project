17580408c35521af29c0d1b640d7b49b5d05cd09
iBekzod <erkinovbegzod.45@gmail.com>
2021-06-16 17:08:03 +0500

    city relation changed

--- app/City.php
+++ app/City.php
@@ -12,6 +12,14 @@ class City extends Model
         return $this->belongsTo(Country::class);
     }
 
+    public function parent()
+    {
+        if($this->parent_id==0){
+            return $this->belongsTo(Country::class);
+        }
+        return $this->belongsTo(City::class);
+    }
+
     public function getTranslation($field = '', $lang = false){
         $lang = $lang == false ? app()->getLocale() : $lang;
         $city_translation = $this->city_translations()->where('lang', $lang)->first();
