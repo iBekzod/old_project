47cc31165d1ed096fc98d4e94cbd34d10c0036d4
Alijon27011995 <muxtorova227@gmail.com>
2021-07-01 19:35:18 +0500

    connect Conversation

--- app/Http/Controllers/SupportTicketController.php
+++ app/Http/Controllers/SupportTicketController.php
@@ -9,7 +9,7 @@ use Auth;
 use App\TicketReply;
 use App\Mail\SupportMailManager;
 use Mail;
-
+use App\Conversation;
 class SupportTicketController extends Controller
 {
     /**
@@ -25,6 +25,11 @@ class SupportTicketController extends Controller
 
     public function admin_index(Request $request)
     {
+        // return 'keldi';
+        $sender_id = auth()->id();
+        // $user = User::findOrFail($user_id);
+        $conversation=Conversation::findOrFail($sender_id);
+        dd($conversation);
         $sort_search =null;
         $tickets = Ticket::orderBy('created_at', 'desc');
         if ($request->has('search')){
