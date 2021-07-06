6c41120f0bb92142236fc59759a05a0e12af9234
iBekzod <erkinovbegzod.45@gmail.com>
2021-07-06 11:32:47 +0500

    file preview changes

--- resources/views/backend/product/elements/create.blade.php
+++ resources/views/backend/product/elements/create.blade.php
@@ -508,7 +508,9 @@
                     // console.log(data.message);
                     if(data.success){
                         $('#variation_div').html(data.data)
-                        $('.js-example-basic-multiple').select2();
+                        AIZ.uploader.initForInput();
+                        AIZ.uploader.removeAttachment();
+                        AIZ.uploader.previewGenerate();
                     }
                 }
             });
