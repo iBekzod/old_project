e695c606ebadce7385f13d2d315242e5e2b5fb1c
iBekzod <erkinovbegzod.45@gmail.com>
2021-08-04 10:09:36 +0500

    flash deal added to product

--- app/FlashDealProduct.php
+++ app/FlashDealProduct.php
@@ -7,8 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class FlashDealProduct extends Model
 {
+    protected $appends = [
+        'flash_deal'
+    ];
     public function product()
     {
         return $this->hasOne(Product::class, 'id', 'product_id');
+    }
+
+    public function getFlashDealAttribute()
+    {
+        return FlashDeal::where('id', $this->flash_deal_id)->first()??[];
     }
 }
