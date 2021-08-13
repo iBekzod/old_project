aa5c53d77f5e9ca09590dc947c9d13d2763585e6
iBekzod <erkinovbegzod.45@gmail.com>
2021-08-13 18:55:12 +0500

    bug fix

--- app/Cart.php
+++ app/Cart.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Cart extends Model
 {
     protected $guarded = [];
-
+    protected $table = 'carts';
     public function user()
     {
         return $this->belongsTo(User::class);
