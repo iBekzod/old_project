423955e2519646177351b34e51378b6aac6b5047
iBekzod <erkinovbegzod.45@gmail.com>
2021-09-02 11:00:52 +0500

    seller details added

--- app/Http/Resources/OrderDetailCollection.php
+++ app/Http/Resources/OrderDetailCollection.php
@@ -15,6 +15,16 @@ class OrderDetailCollection extends ResourceCollection
 //                    'delivery_type' => $data->delivery_type,
                     'price' => $data->price,
                     'product' => $data->product,
+                    'user' => [                        
+                        'seller_id' => $data->product->user->id,
+                        'name' => $data->product->user->name,
+                        'email' => $data->product->user->email,
+                        'avatar' => $data->product->user->avatar,
+                        'avatar_original' => api_asset($data->product->user->avatar_original),
+                        'shop_name' => $data->product->added_by == 'admin' ? '' : $data->product->user->shop->name,
+                        'shop_logo' => $data->product->added_by == 'admin' ? '' : api_asset($data->product->user->shop->logo),
+                        'shop_link' => $data->product->added_by == 'admin' ? '' : route('shops.info', $data->product->user->shop->id)
+                    ],
                 ];
             })
         ];
