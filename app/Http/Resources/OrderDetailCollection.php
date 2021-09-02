70c7f05cacc462dcbca5927361144f8b0cee9f4e
iBekzod <erkinovbegzod.45@gmail.com>
2021-09-02 11:26:52 +0500

    address added to order detail

--- app/Http/Resources/OrderDetailCollection.php
+++ app/Http/Resources/OrderDetailCollection.php
@@ -10,22 +10,33 @@ class OrderDetailCollection extends ResourceCollection
     {
         return [
             'data' => $this->collection->map(function($data) {
+                $product=$data->product;
+                $address=$product->user->addresses->first();
                 return [
                     'quantity' => $data->quantity,
 //                    'delivery_type' => $data->delivery_type,
                     'price' => $data->price,
-                    'product' => $data->product,
+                    'product' => $product,
                     'user' => [                        
-                        'id' => $data->product->user->id,
-                        'phone' => $data->product->user->phone,
-                        'name' => $data->product->user->name,
-                        'email' => $data->product->user->email,
-                        'avatar' => $data->product->user->avatar,
-                        'avatar_original' => api_asset($data->product->user->avatar_original),
-                        'shop_name' => $data->product->added_by == 'admin' ? '' : $data->product->user->shop->name,
-                        'shop_logo' => $data->product->added_by == 'admin' ? '' : api_asset($data->product->user->shop->logo),
-                        'shop_link' => $data->product->added_by == 'admin' ? '' : route('shops.info', $data->product->user->shop->id)
+                        'id' => $product->user->id,
+                        'phone' => $product->user->phone,
+                        'name' => $product->user->name,
+                        'email' => $product->user->email,
+                        'avatar' => $product->user->avatar,
+                        'avatar_original' => api_asset($product->user->avatar_original),
+                        'shop_name' => $product->added_by == 'admin' ? '' : $product->user->shop->name,
+                        'shop_logo' => $product->added_by == 'admin' ? '' : api_asset($product->user->shop->logo),
+                        'shop_link' => $product->added_by == 'admin' ? '' : route('shops.info', $product->user->shop->id),
                     ],
+                    'address'=>[
+                        'address'=>$address->address,
+                        'country'=>$address->region->name,
+                        'city'=>$address->city->name,
+                        'postal_code'=> $address->postal_code,
+                        'phone'=>$address->phone,
+                        'longitude'=>$address->longitude,
+                        'latitude'=>$address->latitude
+                    ]
                 ];
             })
         ];
