24a92110d5b6b41167c3eb36aec6d5e18e3ba8de
iBekzod <erkinovbegzod.45@gmail.com>
2021-09-14 18:01:35 +0500

    cancel request

--- routes/api.php
+++ routes/api.php
@@ -276,10 +276,10 @@ Route::prefix('v2')->group(function () {
         Route::get('earning-summary/{id}', 'Api\V2\DeliveryBoyController@earning_summary')->middleware('auth:api');
         Route::get('collection/{id}', 'Api\V2\DeliveryBoyController@collection')->middleware('auth:api');
         Route::get('earning/{id}', 'Api\V2\DeliveryBoyController@earning')->middleware('auth:api');
-        Route::get('cancel-request/{id}', 'Api\V2\DeliveryBoyController@cancel_request')->middleware('auth:api');
+        Route::get('cancel-request', 'Api\V2\DeliveryBoyController@cancel_request')->middleware('auth:api');
         Route::post('change-delivery-status', 'Api\V2\DeliveryBoyController@change_delivery_status')->middleware('auth:api');
     });
-    
+
     Route::apiResource('reasons', 'Api\ReasonController')->only('index');
     Route::post('refund-request-send', 'Api\RefundRequestController@request_store')->middleware('auth:api');
     Route::get('refund-request', 'Api\RefundRequestController@vendor_index')->middleware('auth:api');
