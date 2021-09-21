bef24f5df2bc41ecb45db79247c13bdc6724bf79
iBekzod <erkinovbegzod.45@gmail.com>
2021-09-21 21:43:41 +0500

    corected CouponUsage date coupon - code

--- app/Http/Resources/CouponUsageCollection.php
+++ app/Http/Resources/CouponUsageCollection.php
@@ -12,7 +12,7 @@ class CouponUsageCollection extends ResourceCollection
             'data' => $this->collection->map(function($data) {
                 return [
                     'id'=>$data->id,
-                    'code'=>[
+                    'coupon'=>[
                         'id'=>$data->coupon->id,
                         'type'=>$data->coupon->type,
                         'code'=>$data->coupon->code,
@@ -24,6 +24,24 @@ class CouponUsageCollection extends ResourceCollection
             })
         ];
     }
+    // public function toArray($request)
+    // {
+    //     return [
+    //         'data' => $this->collection->map(function($data) {
+    //             return [
+    //                 'id'=>$data->id,
+    //                 'code'=>[
+    //                     'id'=>$data->coupon->id,
+    //                     'type'=>$data->coupon->type,
+    //                     'code'=>$data->coupon->code,
+    //                     'start_date'=>date("Y-m-d H:i:s", $data->coupon->start_date),
+    //                     'end_date'=>date("Y-m-d H:i:s", $data->coupon->end_date)
+    //                 ],
+    //                 'created_at'=>$data->created_at
+    //             ];
+    //         })
+    //     ];
+    // }
 
     public function with($request)
     {
