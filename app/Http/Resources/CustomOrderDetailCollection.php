3b701e77b323dc59a20e945bdf3e8ebb6610742d
iBekzod <erkinovbegzod.45@gmail.com>
2021-09-24 21:42:06 +0500

    custom order changed

--- app/Http/Resources/CustomOrderDetailCollection.php
+++ app/Http/Resources/CustomOrderDetailCollection.php
@@ -9,16 +9,16 @@ class CustomOrderDetailCollection extends ResourceCollection
     public function toArray($request)
     {
         return [
-            'data' => $this->collection->map(function($data) {
+            'data' => $this->product->collection->map(function($data) {
                 return [
                     'id' => (integer) $data->id,
                     'product'=>new SingleProductCollection($data->product),
                     'currency_code'=>defaultCurrency(),
                     'exchange_rate'=>defaultExchangeRate(),
-                    'base_price' => (double) homeBasePrice($this->id),
-                    'base_discounted_price' => (double) homeDiscountedBasePrice($this->id),
-                    'discount' => (integer) $this->discount,
-                    'discount_type' => $this->discount_type,
+                    'base_price' => (double) homeBasePrice($data->product->id),
+                    'base_discounted_price' => (double) homeDiscountedBasePrice($data->product->id),
+                    'discount' => (integer) $data->product->discount,
+                    'discount_type' => $data->product->discount_type,
                 ];
             })
         ];
