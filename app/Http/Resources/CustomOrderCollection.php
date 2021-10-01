39efb0abcb78f1d63c68331c72bca4f63906258e
iBekzod <erkinovbegzod.45@gmail.com>
2021-10-01 16:44:16 +0500

    order

--- app/Http/Resources/CustomOrderCollection.php
+++ app/Http/Resources/CustomOrderCollection.php
@@ -15,6 +15,7 @@ class CustomOrderCollection extends ResourceCollection
                     $product=$orderDetail->product;
                     $order_details[]=[
                         'order_detail_id'=>$orderDetail->id,
+                        'product_id'=>$orderDetail->id,
                         'name'=>$product->getTranslation('name'),
                         'slug'=>$product->slug,
                         'price'=>$orderDetail->price,
@@ -22,6 +23,7 @@ class CustomOrderCollection extends ResourceCollection
                         'thumbnail_image' => api_asset($product->variation->thumbnail_img),
                         'quantity'=>$orderDetail->quantity,
                         'shop_name'=>$product->user->shop->name,
+                        'brand_name'=>$product->element->brand->getTranslation('name'),
                         'seller_phone'=>$product->user->phone
                     ];
                 }
@@ -30,11 +32,17 @@ class CustomOrderCollection extends ResourceCollection
                     'code' => $data->code,//code
                     'payment_status' => $data->payment_status,
                     'delivery_status' => $data->delivery_status,//delivery_status
+                    'sub_total'=>$data->orderDetails->sum('price'),
+                    // 'total_tax'=>$data->orderDetails->sum('tax'),
+                    'total_quantity'=>$data->orderDetails->sum('quantity'),
+                    'shipping_cost'=>$data->orderDetails->sum('shipping_cost'),
                     'grand_total' => (double) $data->grand_total,//grnad_total
                     'discount' => $data->coupon_discount,
                     'payment_method' => $data->payment_type,
-                    'shipping_address' => json_decode($data->shipping_address),
                     'date' => formatDate($data->created_at),
+
+                    'shipping_address' => json_decode($data->shipping_address),
+
                     'order_products' => $order_details
                 ];
             })
